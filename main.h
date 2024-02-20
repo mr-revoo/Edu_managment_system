094fa35c6dd552434310b7fdfec8388d4173990d
refaat <refaatmohamed61@gmail.com>
2024-02-20 06:05:44 +0200

    adding the user class

--- main.h
+++ main.h
@@ -1,7 +1,33 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <iostream>
+using namespace std;
+class User{
+    private:
+    string Name;
+    string password;
+    public:
+    User()=default;
+    User(string Name,string password){
+        this->Name=Name;
+        this->password=password;
+    }
+    bool login(const User& other)const{
+        if(Name==other.Name&&password==other.password){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+
+    void logout(){
+        //returning to the main menu of the user
+        cout<<"Logging out ..."<<endl;
+    }
 
 
+
+};
 
 
 
