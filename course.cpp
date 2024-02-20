d3f28f8f5babcf05e244225fc3624753826ff0dd
refaat <refaatmohamed61@gmail.com>
2024-02-20 16:36:59 +0200

    making a createcourse method and initialzing the listcourses method

--- course.cpp
+++ course.cpp
@@ -9,6 +9,15 @@ class course{
 public:
     void printcoursedata(){
         cout<<courseName<<" "<<courseCode<<endl;
-    }    
+    }
+    void Createcourse(string courseName,string courseCode){
+        this->courseName=courseName;
+        this->courseCode=courseCode;
+        cout<<"Course : "<<courseName<<" with course code : "<<courseCode<<"Has added Successfully!"<<endl;
+    }
+
+    void Listcourses(){
+
+    }
 
 };
